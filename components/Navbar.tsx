120dab86bbe971419c971ff85e28e0834cd8a1ff
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2023-08-25 08:58:36 +0700

    done layout

---
+++ components/Navbar.tsx
@@ -0,0 +1,27 @@
+import Image from "@/node_modules/next/image";
+import Link from "@/node_modules/next/link";
+import CustomButton from "./CustomButton";
+
+const Navbar = () => {
+  return (
+    <header className=" pt-4 w-full absolute z-10">
+      <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent">
+        <Link href="/" className="flex justify-center items-center">
+          <Image
+            src="/logo.svg"
+            alt="logo"
+            width={118}
+            height={18}
+            className="object-contain"
+          />
+        </Link>
+        <CustomButton
+          title="Sign in"
+          containerStyles="text-primary-blue rounded-full bg-white min-w-[130px]"
+        />
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
