e2f58045737da3179ab66b7b18be5a8601411c9f
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2023-08-25 16:17:02 +0700

    Car Catalogue

--- types/index.ts
+++ types/index.ts
@@ -1,7 +1,12 @@
+import { manufacturers } from "./../constants/index";
 import { MouseEventHandler } from "react";
 
 export interface CustomButtonProps {
   title: string;
   containerStyles?: string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
+}
+export interface SearchManuFacturer {
+  manufacturer: string;
+  setManufacturer: (manufacturer: string) => void;
 }
